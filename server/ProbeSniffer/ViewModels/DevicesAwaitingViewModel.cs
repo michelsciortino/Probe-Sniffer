5f44545fcc9327e9f3625a4e133a073d955a1e18
Michel Sciortino <michel.sciortino@outlook.com>
2018-04-30 00:29:48 +0200

    ViewModel for device awaiting SplashScreen view

---
+++ server/ProbeSniffer/ViewModels/DevicesAwaitingViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models;
+using Core.ViewModelBase;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbeSniffer.ViewModels
+{
+    public class DevicesAwaitingViewModel : BaseViewModel
+    {
+        private ObservableCollection<Device> _items=null;
+        public ObservableCollection<Device> Items
+        {
+            get =>_items;
+            set { if (_items == value) return; _items = value; OnPropertyChanged(nameof(Items)); }
+        }
+        public DevicesAwaitingViewModel(ObservableCollection<Device> devices)
+        {
+            Items = devices;
+        }
+    }
+}
