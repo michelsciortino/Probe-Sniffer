1456646df94a418814fafef9941f06256d79fa8b
Michel Sciortino <michel.sciortino@outlook.com>
2018-04-29 20:48:10 +0200

    BaseViewModel classes

---
+++ server/Core/ViewModelBase/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Core.ViewModelBase
+{
+    /// <summary>
+	/// A base view model that fires Property Changed events as needed
+	/// </summary>
+	public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        ///Call this to fire a<see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
