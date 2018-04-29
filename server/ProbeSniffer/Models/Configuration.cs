31d61e806ea42525bfc4fb7044b728b2021a485d
Michel Sciortino <michel.sciortino@outlook.com>
2018-04-29 13:11:43 +0200

    Configuration Class created

---
+++ server/ProbeSniffer/Models/Configuration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbeSniffer.Models
+{
+    [Serializable]
+    public class Configuration
+    {
+        #region Private
+
+        private int _nDevices = 0;
+        private List<Device> _devices = null;
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Configuration()
+        {
+            this._devices = new List<Device>();
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Adds a device to the configuration
+        /// </summary>
+        /// <param name="device">A device to add</param>
+        public void AddDevice(Device device)
+        {
+            _devices.Add(device);
+            _nDevices++;
+        }
+
+        /// <summary>
+        /// Getter for the number of devices in the configuration
+        /// </summary>
+        /// <returns>The number of devices</returns>
+        public int GetNumOfDevices() => _nDevices;
+        #endregion
+    }
+}
---
+++ server/ProbeSniffer/Models/Device.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbeSniffer.Models
+{
+    [Serializable]
+    public class Device
+    {
+        public string MAC { get; set; }
+        public int X_position { get; set; }
+        public int Y_Position { get; set; }
+    }
+}
