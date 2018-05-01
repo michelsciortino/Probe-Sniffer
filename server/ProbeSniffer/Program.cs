1fda4b5b7fbd07406724a3554aac033b6ced49f6
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-02 00:08:00 +0200

    Added Database Connection

--- server/ProbeSniffer/Program.cs
+++ server/ProbeSniffer/Program.cs
@@ -2,12 +2,16 @@
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using Core.Models;
+using Core.DatabaseConnection;
+using System.Windows;
+using System.Collections.Generic;
 
 namespace ProbeSniffer
 {
     public class Program
     {
-        private static Configuration configuration = null;
+        private Configuration configuration = null;
+        private DatabaseConnection DBconnection;
 
         #region Windows
         private SplashScreen splash = null;
@@ -31,29 +35,47 @@ namespace ProbeSniffer
                 new Device { Active = false }
             };            
 
-            //splash = new SplashScreen();
+            splash = new SplashScreen();
 
-            //splash.Show();
+            splash.Show();
 
             //Loading configuration
-            //splash.ShowConfLoadingSplashScreen();
+            splash.ShowConfLoadingSplashScreen();
             configuration = Configuration.LoadConfiguration();
 
-            await Task.Run(async () => Thread.Sleep(5000));
+            //await Task.Run(async () => Thread.Sleep(5000));
 
             //Test Database connection
-            //splash.ShowDBConneLoadingSplashScreen();
-            /* connect()....*/
+            splash.ShowDBConneLoadingSplashScreen();
+            DBconnection = new DatabaseConnection();
+            int tries = 0;
+            while (tries!=3)
+            {
+                DBconnection.Connect();
+                if (DBconnection.Connected) break;
+                //splash.ShowDBConnectionErrorRetrying();
+                Thread.Sleep(2000);
+                tries++;
+            }
+
+            if (!DBconnection.Connected)    //not connected after 3 tries
+            {
+                MessageBox.Show("Unable to connect to Database...\nExiting.", "Error", MessageBoxButton.OK,MessageBoxImage.Error,MessageBoxResult.None,MessageBoxOptions.DefaultDesktopOnly);
+                splash.Close();
+                Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+                Application.Current.Shutdown();
+                return;
+            }
+            
 
             await Task.Run(async () => Thread.Sleep(5000));
-
             //Connecting to Device
-            //splash.ShowDeviceAwaitingSplashScreen();
+            splash.ShowDeviceAwaitingSplashScreen();
             //DeviceCommunication.Initialize();
 
             await Task.Run(async () => Thread.Sleep(5000));
 
-            //splash.Close();
+            splash.Close();
             //visualizer = new DataVisualizer(_devices);
             //visualizer.Show();
         }
