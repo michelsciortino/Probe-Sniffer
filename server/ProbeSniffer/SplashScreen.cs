8d03a5dfa8caa8ae682399fcae7f5642dfdcbd53
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-01 10:42:46 +0200

    Some Changes

--- server/ProbeSniffer/SplashScreen.cs
+++ server/ProbeSniffer/SplashScreen.cs
@@ -1,15 +1,16 @@
 ﻿using System.Threading;
 using System.Windows.Threading;
 using ProbeSniffer.Views;
-using ProbeSniffer.ViewModels;
 using ProbeSniffer.Windows;
+using ProbeSniffer.ViewModels.SplashScreen;
+using System;
 
 namespace ProbeSniffer
 {
     public class SplashScreen
     {
         private Thread SplashThread = null;
-        private SplashWindow splashWindow = null;
+        private SplashScreenWindow splashWindow = null;
         
         /// <summary>
         /// Shows the Splash Screen
@@ -18,14 +19,16 @@ namespace ProbeSniffer
         {
             SplashThread = new Thread(new ThreadStart(() =>
             {
-                splashWindow = new SplashWindow();
+                splashWindow = new SplashScreenWindow();
                 splashWindow.Show();
                 Dispatcher.Run();
+
             }));
             SplashThread.Priority = ThreadPriority.AboveNormal;
             SplashThread.SetApartmentState(ApartmentState.STA);
             SplashThread.IsBackground = false;
             SplashThread.Start();
+            Thread.Sleep(1000);
         }
 
         /// <summary>
@@ -34,6 +37,7 @@ namespace ProbeSniffer
         public void ShowConfLoadingSplashScreen()
         {
             Dispatcher dispatcher = Dispatcher.FromThread(SplashThread);
+            if (dispatcher == null) throw new Exception("SplashScreen Dispatcher not running");
             dispatcher.Invoke(() =>
             {
                 ((SplashViewModel)splashWindow.DataContext).CurrentPage= new ConfigurationLoadingView();
@@ -46,10 +50,17 @@ namespace ProbeSniffer
         /// </summary>
         public void ShowDBConneLoadingSplashScreen()
         {
-            Dispatcher.FromThread(SplashThread).Invoke(() =>
+            Dispatcher dispatcher = Dispatcher.FromThread(SplashThread);
+            if (dispatcher == null) throw new Exception("SplashScreen Dispatcher not running");
+            dispatcher.Invoke(() =>
             {
                 ((SplashViewModel)splashWindow.DataContext).CurrentPage = new DatabaseConnectionLoadingView();
             });
+        }
+
+        public void Close()
+        {
+            SplashThread.Abort();
         }
 
         /// <summary>
@@ -57,7 +68,9 @@ namespace ProbeSniffer
         /// </summary>
         public void ShowDeviceAwaitingSplashScreen()
         {
-            Dispatcher.FromThread(SplashThread).Invoke(() =>
+            Dispatcher dispatcher = Dispatcher.FromThread(SplashThread);
+            if (dispatcher == null) throw new Exception("SplashScreen Dispatcher not running");
+            dispatcher.Invoke(() =>
             {
                 ((SplashViewModel)splashWindow.DataContext).CurrentPage = new DevicesAwaitingView();
             });
