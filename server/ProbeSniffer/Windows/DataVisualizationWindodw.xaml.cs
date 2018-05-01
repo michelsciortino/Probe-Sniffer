a202dc820b4d6a9e51a5d8bcb1dd076682f57b41
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-01 10:39:29 +0200

    New Data Visualization Window

---
+++ server/ProbeSniffer/Windows/DataVisualizationWindodw.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ProbeSniffer.Windows
+{
+    /// <summary>
+    /// Logica di interazione per DataVisualizer.xaml
+    /// </summary>
+    public partial class DataVisualizationWindow : Window
+    {
+        public DataVisualizationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
