c2ceee643eb1960da84a9a17608ea7168626d6a2
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-02 00:07:39 +0200

    Database entry structure

---
+++ server/Core/Models/DatabaseEntry.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class DatabaseEntry
+    {
+        public ObjectId Id { get; set; }
+        public int IntervalId { get; set; }
+        public Device Device { get; set; }
+        public string SSID { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Hash { get; set; }
+    }
+}
