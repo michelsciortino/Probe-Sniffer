3b774b52b1c2829028f2759c167e2700d50b60f0
Andrea Mora <andrea.mora@outlook.it>
2018-05-02 18:37:12 +0200

    Messages Definitions

---
+++ server/Core/Models/DeviceMessages/Ok_Message.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Models.DeviceMessages
+{
+    public class Ok_Message
+    {
+        public string Message { get; } = "OK";
+    }
+}
---
+++ server/Core/Models/DeviceMessages/Server_Advertisement.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Core.Models.DeviceMessages
+{
+    class Server_Advertisement
+    {
+        public string Message => AdvertisementCreator();
+
+        private static string AdvertisementCreator()
+        {
+            string message = "";
+
+            IPAddress localIP = LocalNetworkConnection.GetLocalIp();
+            message = "SERVER_ADV" + localIP.ToString();
+
+            return message;
+        }
+    }
+}
