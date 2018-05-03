f2e2ff6760d978607eefba9c11d806568d8c522f
Andrea Mora <andrea.mora@outlook.it>
2018-05-03 13:04:53 +0200

    Dummy garbage class

---
+++ server/Core/Utilities/Utilities.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Utilities
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Stretches a string to <paramref name="lenght"/>.
+        /// </summary>
+        /// <param name="original">Original string</param>
+        /// <param name="lenght">Desired lenght</param>
+        /// <returns>The original string stretched to <paramref name="lenght"/> with " ".</returns>
+        public static string Stretch(string original, int lenght)
+        {
+            string output = original;
+            for (int i = original.Length; i < lenght; i++)
+                output += " ";
+            return output;
+        }
+    }
+}
