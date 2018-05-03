e0b3e6bc80c8268822bc63d26959574c0c54edd0
Andrea Mora <andrea.mora@outlook.it>
2018-05-03 13:09:42 +0200

    UDPsender class implemented

---
+++ server/Core/DeviceCominication/UDPsender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Core.DeviceCominication
+{
+    public static class UDPsender
+    {
+        public static bool Send(IPAddress dest,int port,string packet)
+        {
+            try
+            {
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(packet.ToCharArray(), 0, packet.Length);
+                UdpClient client = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(dest, port);
+                client.EnableBroadcast = true;
+                client.Send(sendBytes, sendBytes.Length, endPoint);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+    }
+}
