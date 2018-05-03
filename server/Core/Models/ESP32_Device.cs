f07d72855fa1349ec78e03f58b794473a1a317f8
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-03 17:28:17 +0200

    Added Constructors

--- server/Core/Models/ESP32_Device.cs
+++ server/Core/Models/ESP32_Device.cs
@@ -4,7 +4,21 @@ namespace Core.Models
 {
     public class ESP32_Device : Device
     {
-        public IPAddress Ip { get; set; } = null;
-        public bool Active { get; set; } = false;
+        public IPAddress Ip { get; set; }
+        public bool Active { get; set; }
+
+        public ESP32_Device()
+        {
+            Ip = null;
+            Active = false;
+        }
+        public ESP32_Device(Device device)
+        {
+            MAC = device.MAC;
+            X_position = device.X_position;
+            Y_Position = device.Y_Position;
+            Ip = null;
+            Active = false;
+        }
     }
 }
