dac03c4932e2d61cd584a4674f43f18dd5a42023
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-03 16:58:29 +0200

    Defined ESP32_Device

---
+++ server/Core/Models/ESP32_Device.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Core.Models
+{
+    public class ESP32_Device : Device
+    {
+        public IPAddress Ip { get; set; } = null;
+        public bool Active { get; set; } = false;
+    }
+}
