fa65fc9cc5a36dbf370529d3710746b60fac6815
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-03 22:01:16 +0200

    Server Messages specifications

---
+++ server/Core/DeviceCommunication/Messages/Server Messages/Server_Advertisement_Message.cs
@@ -0,0 +1,28 @@
+﻿using Core.Models;
+using Core.Utilities;
+using System.Net;
+
+namespace Core.DeviceCommunication.Messages.Server_Messages
+{
+    public class Server_Advertisement_Message : Server_Message
+    {
+        public const string SERVER_ADV = "SERVER_ADV";
+
+        #region Constructor
+        public Server_Advertisement_Message()
+        {
+            IPAddress localIP = LocalNetworkConnection.GetLocalIp();
+            string ip = localIP.ToString();
+            _header = SERVER_ADV;
+            _payload = ip.Length.ToString() + ip;
+        }
+        #endregion
+
+        #region Public Methods
+        public override string ToString()
+        {
+            return Util.Stretch(_header, 10) + _payload;
+        }
+        #endregion
+    }
+}
---
+++ server/Core/DeviceCommunication/Messages/Server Messages/Timestamp_Message.cs
@@ -0,0 +1,25 @@
+﻿using Core.Utilities;
+using System;
+
+namespace Core.DeviceCommunication.Messages.Server_Messages
+{
+    public class Timestamp_Message : Server_Message
+    {
+        public const string TIMESTAMP_HEADER = "TIMESTAMP";
+        #region Constructor
+        public Timestamp_Message()
+        {
+            _header = TIMESTAMP_HEADER;
+        }
+        #endregion
+
+        #region Public Properties
+        public new string _payload => DateTime.UtcNow.ToString();
+        #endregion
+
+        #region Public Methods
+        public override string ToString() => Util.Stretch(_header, header_lenght) + _payload.Length + _payload;
+
+        #endregion
+    }
+}
