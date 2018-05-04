850bd0f144898e022406ba3310b79ea986482f85
Andrea Mora <andrea.mora@outlook.it>
2018-05-04 02:09:47 +0200

    First implementation of server TCP for READY messages (not perfect)

--- server/Core/DeviceCommunication/ESPManager.cs
+++ server/Core/DeviceCommunication/ESPManager.cs
@@ -1,5 +1,7 @@
-﻿using Core.Models;
+﻿using System;
 using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Core.DeviceCommunication
 {
@@ -7,6 +9,59 @@ namespace Core.DeviceCommunication
     {
         public static bool ReceiveDeviceReady(ref string mac, ref IPAddress ip)
         {
+            TcpListener server = null;
+
+            try
+            {
+                int port = DeviceCommunication.SERVER_PORT;   //porta dove ricevere
+                IPAddress LocalIP = Core.Models.LocalNetworkConnection.GetLocalIp(); //mettere ip dove ricevere
+
+                server = new TcpListener(LocalIP, port);
+
+                server.Start();
+
+                Byte[] bytesbuffer = new Byte[27];  //10(header)+17(mac)    METTERE COME CAMPO NELLE CLASSI MESSAGGI LA LUNGHEZZA FINALE SE C'E'
+                string data = null;
+
+                //listening loop
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connection...");
+                    TcpClient device = server.AcceptTcpClient();
+                    Console.WriteLine("connected!");
+
+                    data = null;
+
+                    //faccio uno stream per leggere
+                    NetworkStream stream = device.GetStream();
+                    int i;
+
+                    //loop to receive data
+                    while ((i = stream.Read(bytesbuffer, 0, bytesbuffer.Length)) != 0)
+                    {
+                        data = Encoding.ASCII.GetString(bytesbuffer, 0, i);
+                        Console.WriteLine("received: ", data);
+
+                        //Estraggo MAC da data
+                        //da aggiustare non va
+                        mac = data.Substring(11, 17);
+
+                        //mandiamo già quà OK? o lasciamo fuori?
+                        //byte[] ok = Encoding.ASCII.GetBytes("ok");
+                        //stream.Write(ok,0,ok.Length);
+                    }
+                    device.Close();
+                }
+                return true;
+            }
+            catch (SocketException e)
+            {
+                return false;
+            }
+            finally
+            {
+                server.Stop();
+            }
             return true;
         }
     }
