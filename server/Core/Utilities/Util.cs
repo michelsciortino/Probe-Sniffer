d3977aa5262051bc84f0501323960e13ab04e392
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-05 20:33:56 +0200

    Revert "Dummy garbage class"
    This reverts commit 87dab68ee45738079b3afd59b8380fd166e6d74b.

--- server/Core/Utilities/Util.cs
+++ server/Core/Utilities/Util.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Text;
-
-namespace Core.Utilities
+﻿namespace Core.Utilities
 {
     public static class Util
     {
         /// <summary>
-        /// Concat header in byte with paylod in bytes
+        /// Stretches a string to <paramref name="lenght"/>.
         /// </summary>
-        /// <param name="header">Header in byte</param>
-        /// <param name="payload">Payload to convert in bytes and concat</param>
-        /// <returns>Return message in bytes</returns>
-        public static byte[] MessageInBytes(byte header, string payload)
+        /// <param name="original">Original string</param>
+        /// <param name="lenght">Desired lenght</param>
+        /// <returns>The original string stretched to <paramref name="lenght"/> with " ".</returns>
+        public static string Stretch(string original, int lenght)
         {
-            byte[] message = new Byte[payload.Length+1];
-            message[0] = header;
-            byte[] payloadBytes = Encoding.ASCII.GetBytes(payload.ToCharArray(), 0, payload.Length);
-            Buffer.BlockCopy(message, 0, payloadBytes, 1, payloadBytes.Length);
-            return message;
+            string output = original;
+            for (int i = original.Length; i < lenght; i++)
+                output += " ";
+            return output;
         }
     }
 }
