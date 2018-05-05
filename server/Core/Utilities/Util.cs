8b6e41c0fe7b93efeb4eb4e9dfc7ca038ae77a74
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-05 21:34:54 +0200

    Clean up

--- server/Core/Utilities/Util.cs
+++ server/Core/Utilities/Util.cs
@@ -1,23 +1,6 @@
-﻿using System;
-using System.Text;
-
-namespace Core.Utilities
+﻿namespace Core.Utilities
 {
     public static class Util
     {
-        /// <summary>
-        /// Concat header in byte with paylod in bytes
-        /// </summary>
-        /// <param name="header">Header in byte</param>
-        /// <param name="payload">Payload to convert in bytes and concat</param>
-        /// <returns>Return message in bytes</returns>
-        public static byte[] MessageInBytes(byte header, string payload)
-        {
-            byte[] message = new Byte[payload.Length + 1];
-            message[0] = header;
-            byte[] payloadBytes = Encoding.ASCII.GetBytes(payload.ToCharArray(), 0, payload.Length);
-            Buffer.BlockCopy(message, 0, payloadBytes, 1, payloadBytes.Length);
-            return message;
-        }
     }
 }
