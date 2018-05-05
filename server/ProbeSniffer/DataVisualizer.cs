2fc5024287c751d5901908f1099600f0afb9bbb2
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-05 18:37:04 +0200

    Added a State to the Data visualizer

--- server/ProbeSniffer/DataVisualizer.cs
+++ server/ProbeSniffer/DataVisualizer.cs
@@ -1,18 +1,27 @@
 ﻿using Core.Models;
 using ProbeSniffer.Windows;
+using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Windows;
 using System.Windows.Threading;
 
 namespace ProbeSniffer
 {
     public class DataVisualizer
     {
+        private enum VisualizerState
+        {
+            CLOSED = 0,
+            OPEN,
+            HIDDEN
+        }
+
         #region Private
-        private bool _isShown = false;
         private Thread VisualizerThread = null;
         private DataVisualizationWindow _dataVisualizationWindow = null;
         private List<Device> _devices=null;
+        private VisualizerState state = VisualizerState.CLOSED;
         #endregion
 
         #region Constructor
@@ -26,31 +35,53 @@ namespace ProbeSniffer
         #region Public Methods
         public void Show()
         {
-            if (_isShown) return;
-            VisualizerThread = new Thread(new ThreadStart(() =>
+            if (state == VisualizerState.CLOSED)
             {
-                _dataVisualizationWindow = new DataVisualizationWindow();
-                _dataVisualizationWindow.Show();
-                Dispatcher.Run();
-            }));
-            VisualizerThread.Priority = ThreadPriority.AboveNormal;
-            VisualizerThread.SetApartmentState(ApartmentState.STA);
-            VisualizerThread.IsBackground = false;
-            VisualizerThread.Start();
-            _isShown = true;
+                VisualizerThread = new Thread(new ThreadStart(() =>
+                {
+                    _dataVisualizationWindow = new DataVisualizationWindow();
+                    _dataVisualizationWindow.Show();
+                    _dataVisualizationWindow.Closed += VisualizerClosed;
+                    Dispatcher.Run();
+                }));
+                VisualizerThread.Priority = ThreadPriority.AboveNormal;
+                VisualizerThread.SetApartmentState(ApartmentState.STA);
+                VisualizerThread.IsBackground = false;
+                VisualizerThread.Start();
+                while (true)
+                {
+                    Dispatcher dispatcher = Dispatcher.FromThread(VisualizerThread);
+                    if (dispatcher == null)
+                        Thread.Sleep(100);
+                    else
+                        break;
+                }
+            }
+            else
+            {
+                Dispatcher dispatcher = Dispatcher.FromThread(VisualizerThread);
+                dispatcher.Invoke(() => _dataVisualizationWindow.Activate()) ;
+            }
+            state = VisualizerState.OPEN;
+        }
+
+        private void VisualizerClosed(object sender, EventArgs e)
+        {
+            state = VisualizerState.CLOSED;
         }
 
         public void Hide()
         {
-            if (!_isShown) return;
+            if (state == VisualizerState.CLOSED || state == VisualizerState.HIDDEN) return;
+
             Dispatcher VisualizerDispatcher = Dispatcher.FromThread(VisualizerThread);
             if (VisualizerThread == null)
                 throw new System.Exception("The visualizer dispatcher was not ready");
             VisualizerDispatcher.Invoke(() =>
             {
-                _dataVisualizationWindow.Close();
+                _dataVisualizationWindow.Hide();
             });
-            _isShown = false;
+            state = VisualizerState.HIDDEN;
         }
         #endregion
     }
