2c602809d62552209aeece3084e123ec2e06464a
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-05 18:34:45 +0200

    Added the About window

--- server/ProbeSniffer/ViewModels/DataVisualizer/DataVisualizationWindowViewModel.cs
+++ server/ProbeSniffer/ViewModels/DataVisualizer/DataVisualizationWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Core.ViewModelBase;
 using ProbeSniffer.Views.DataVisualizer;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -8,6 +9,10 @@ namespace ProbeSniffer.ViewModels.DataVisualizer
     public class DataVisualizationWindowViewModel : BaseViewModel
     {
         #region Private
+        private const string AboutMessage = "Created by:\n\tMichel Sciortino\n\tAndrea Mora\n\tAntonio Monteanni\n\nDesigned by Michel Sciortino.";
+        private const string AboutCaption = "Probe Sniffer";
+
+
         private Page _currentPage = null;
         private LiveViewViewModel _liveViewVM = null;
         private StatisticsViewModel _statisticsVM = null;
@@ -22,24 +27,34 @@ namespace ProbeSniffer.ViewModels.DataVisualizer
             _statisticsVM = new StatisticsViewModel();
             _hiddenDevicesVM = new HiddenDevicesViewModel();
             _sliderViewVM = new SliderViewViewModel();
+            _currentPage = new LiveViewView(_liveViewVM);
         }
         #endregion
 
         #region Public Properties
         public Page CurrentPage {
             get => _currentPage;
-            set { if (_currentPage == value) return; _currentPage = value; OnPropertyChanged(nameof(CurrentPage)); }
+            set
+            {
+                if (_currentPage == value)
+                    return;
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+            }
         }
 
         #endregion
 
         #region Private Commands
         private ICommand _selectionCommand = null;
+        private ICommand _aboutCommand = null;
         #endregion
 
         #region Public Commands
         public ICommand SelectionCommand => _selectionCommand ??
                                             (_selectionCommand = new RelayCommand<object>((x) => Navigate((SelectionChangedEventArgs)x)));
+        public ICommand AboutCommand => _aboutCommand ??
+                                        (_aboutCommand = new RelayCommand<object>((x) => MessageBox.Show(AboutMessage, AboutCaption, MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.None,MessageBoxOptions.ServiceNotification)));
         #endregion
 
         #region Private Methods
