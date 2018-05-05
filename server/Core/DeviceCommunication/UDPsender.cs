374eebf02d7d831056101c65e6f69075500bf5c7
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-05 20:31:49 +0200

    Revert "Fixed after change header message in byte"
    This reverts commit 60aa59a24130022258fd86ff96b551f629b58366.

--- server/Core/DeviceCommunication/UDPsender.cs
+++ server/Core/DeviceCommunication/UDPsender.cs
@@ -2,21 +2,23 @@
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Core.DeviceCommunication
 {
     public static class UDPsender
     {
-        public static bool Send(IPAddress dest, int port, byte[] packet)
+        public static bool Send(IPAddress dest,int port,string packet)
         {
             try
             {
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(packet.ToCharArray(), 0, packet.Length);
                 UdpClient client = new UdpClient();
                 IPEndPoint endPoint = new IPEndPoint(dest, port);
                 client.EnableBroadcast = true;
-                client.Send(packet, packet.Length, endPoint);
+                client.Send(sendBytes, sendBytes.Length, endPoint);
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 Debug.WriteLine(ex.Message);
                 return false;
