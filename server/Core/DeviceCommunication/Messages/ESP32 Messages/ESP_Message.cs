af4d371e5aa6be549c83e481a81443c224cd15ef
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-06 12:44:04 +0200

    ESP messages now contain the IP address of the ESP device which sent the message

--- server/Core/DeviceCommunication/Messages/ESP32 Messages/ESP_Message.cs
+++ server/Core/DeviceCommunication/Messages/ESP32 Messages/ESP_Message.cs
@@ -1,10 +1,13 @@
-﻿namespace Core.DeviceCommunication.Messages.ESP32_Messages
+﻿using System.Net;
+
+namespace Core.DeviceCommunication.Messages.ESP32_Messages
 {
     public abstract class ESP_Message
     {
         #region Private Properties
         protected byte _header;
         protected string _payload;
+        protected IPAddress _espIpAddress;
         #endregion
 
         #region Public Properties
@@ -17,6 +20,11 @@
         {
             get => _payload;
             set => _payload = value;
+        }
+        public IPAddress EspIPAddress
+        {
+            get => _espIpAddress;
+            set => _espIpAddress = value;
         }
         #endregion
     }
