2fb5e63335f0f76c3cd3c02f1501422f35023612
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-06 08:21:44 +0200

    DeviceData Json serialization/deserialization

--- server/Core/Models/DeviceData.cs
+++ server/Core/Models/DeviceData.cs
@@ -1,9 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Core.Models
 {
     public class DeviceData
     {
         public ICollection<Packet> Packets { get; set; }
+
+        public DeviceData()
+        {
+            Packets = new List<Packet>();
+        }
+
+        public DeviceData(string json)
+        {
+            ICollection<Packet> data = null;
+            try
+            {
+                data = JsonConvert.DeserializeObject<ICollection<Packet>>(json);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error deserializing data.", ex);
+            }
+            Packets = data;
+        }
+
+        public string GetJson()
+        {
+            string json = "";
+            try
+            {
+                json = JsonConvert.SerializeObject(Packets);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error serializing data.", ex);
+            }
+            return json;
+        }
     }
 }
