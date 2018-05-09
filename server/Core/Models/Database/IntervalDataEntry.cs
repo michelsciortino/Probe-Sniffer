bcb507ab4dc73e545948a56ae9d5ea14b31974bd
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-10 00:44:05 +0200

    Modified Database entries structure

---
+++ server/Core/Models/Database/IntervalDataEntry.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+
+namespace Core.Models.Database
+{
+    public class IntervalDataEntry
+    {
+        public ObjectId Id { get; set; }
+        public int IntervalId { get; set; }
+        public Device Sender { get; set; }
+        public string SSID { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Hash { get; set; }
+    }
+}
---
+++ server/Core/Models/Database/IntervalDescriptionEntry.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+
+namespace Core.Models.Database
+{
+    public class IntervalDescriptionEntry
+    {
+        public ObjectId Id { get; set; }
+        public int IntervalId { get; set; }
+        public List<Device> ActiveEsps { get; set; }
+        public int NumberOfDetectedDevices { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
