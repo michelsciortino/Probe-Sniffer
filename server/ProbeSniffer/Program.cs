3aae993314bbb6c5ebc02cd1745e015ca09bdd4f
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-23 20:47:39 +0200

    Updated to the new server implementation

--- server/ProbeSniffer/Program.cs
+++ server/ProbeSniffer/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System.Threading;
 using Core.Models;
 using Core.DBConnection;
-using System.Windows;
 using Core.DeviceCommunication;
+using System.Windows;
+using Core.DataCollection;
 
 namespace ProbeSniffer
 {
     public class Program
     {
         private Configuration configuration = null;
-        private DatabaseConnection dbConnection = null;
-        private DeviceConnectionManager deviceCommunication = null;
+        private DataCollector dataCollector = null;
+        //private DeviceConnectionManager deviceCommunication = null;
 
         #region Windows
         private SplashScreen splash = null;
@@ -22,7 +23,7 @@ namespace ProbeSniffer
         /// </summary>
         public void Main()
         {
-            bool result = false;
+            //bool result = false;
 
             //Showing Splash Screen
             splash = new SplashScreen();
@@ -36,14 +37,16 @@ namespace ProbeSniffer
             {
                 ShowErrorMessage("Unable to load the configuration...\nExiting.");
                 splash.Close();
-                Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+                try { Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown; } catch { }
                 Application.Current.Shutdown();
                 return;
             }
+            ESPManager.Initialize(configuration.Devices);
 
+            
             //Testing Database connection
             splash.ShowDBConneLoadingSplashScreen();
-            dbConnection = new DatabaseConnection();
+            DatabaseConnection dbConnection = new DatabaseConnection();
             int tries = 0;
             while (tries != 3)
             {
@@ -56,40 +59,27 @@ namespace ProbeSniffer
             {
                 ShowErrorMessage("Unable to connect to Database...\nExiting.");
                 splash.Close();
-                Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+                try { Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown; } catch { }
                 Application.Current.Shutdown();
                 return;
             }
-
 
             //Connecting to Devices
             splash.ShowDeviceAwaitingSplashScreen();
-            deviceCommunication = new DeviceConnectionManager();
-            result = deviceCommunication.Initialize(configuration.Devices);
-
-            if (result is false)
-            {
-                ShowErrorMessage("Unable to initialize ESP32 devices...\nExiting.");
-                splash.Close();
-                Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
-                Application.Current.Shutdown();
-                return;
-            }
+            dataCollector.Initialize();
+            dataCollector.StartDataCollection();
 
             //Opening visualizer
             splash.Close();
-            visualizer = new DataVisualizer(configuration.Devices);
+            
+            visualizer = new DataVisualizer(configuration?.Devices);
             visualizer.Show();
         }
         
         private void ShowErrorMessage(string message)
         {
-            MessageBox.Show(message,
-                            "Error",
-                            MessageBoxButton.OK,
-                            MessageBoxImage.Error,
-                            MessageBoxResult.None,
-                            MessageBoxOptions.DefaultDesktopOnly);
+            Core.Controls.MessageBox errorBox = new Core.Controls.MessageBox(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None);
+            errorBox.Show();
         }
 
     }
