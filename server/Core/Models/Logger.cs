2cd8dc62b7b2969df23c0f460a0dedc17baa6e62
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-23 20:37:53 +0200

    [New] Logger class

---
+++ server/Core/Models/Logger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Core.Models
+{
+    public static class Logger
+    {
+        #region Private Members
+        private static Queue<string> log = null;
+        private static Mutex logMutex = null;
+        #endregion
+        
+        static Logger()
+        {
+            log = new Queue<string>();
+            logMutex = new Mutex();
+        }
+
+        public static void Log(string message)
+        {
+            logMutex.WaitOne();
+            try
+            {
+                log.Enqueue("[" + DateTime.Now.ToShortTimeString() + "] " + message);
+            }
+            catch
+            {
+                log = new Queue<string>();
+            }
+            logMutex.ReleaseMutex();
+        }
+
+        public static Queue<string> GetLogMessages()
+        {
+            Queue<string> ret = new Queue<string>();
+            logMutex.WaitOne();
+            try
+            {
+                while (log.Count > 0) ret.Enqueue(log.Dequeue());
+            }
+            catch
+            {
+                log = new Queue<string>();
+            }
+            logMutex.ReleaseMutex();
+            return ret;
+        }
+    }
+}
