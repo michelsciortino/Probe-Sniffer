fc91270acee2847a4b4d35f098e6a6744f403a53
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-23 20:44:29 +0200

    New static Colors class

---
+++ server/Core/Styles/Colors.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media;
+
+namespace Core.Styles
+{
+    public static class Colors
+    {
+        public static Color GreenColor = Color.FromArgb(255, 106, 255, 243);
+        public static SolidColorBrush GreenColorBrush = new SolidColorBrush(GreenColor);
+        
+        public static Color DarkGreenColor = Color.FromArgb(255, 64, 153, 146);
+        public static SolidColorBrush DarkGreenColorBrush = new SolidColorBrush(DarkGreenColor);
+        
+        public static Color VioletColor = Color.FromArgb(255, 191, 113, 255);
+        public static SolidColorBrush VioletColorBrush = new SolidColorBrush(VioletColor);
+        
+        public static Color GreyColor = Color.FromArgb(255, 145, 147, 168);
+        public static SolidColorBrush GreyColorBrush = new SolidColorBrush(GreyColor);
+        
+        public static Color YellowColor = Color.FromArgb(255, 255, 243, 142);
+        public static SolidColorBrush YellowColorBrush = new SolidColorBrush(YellowColor);
+        
+        public static Color AzureColor = Color.FromArgb(255, 28, 191, 255);
+        public static SolidColorBrush AzureColorBrush = new SolidColorBrush(AzureColor);
+         
+        public static Color BackgroundColor = Color.FromArgb(255, 48, 48, 48);
+        public static SolidColorBrush BackgroundColorBrush = new SolidColorBrush(BackgroundColor);
+        
+        public static Color BackgroundLighterColor = Color.FromArgb(255, 64, 64, 64);
+        public static SolidColorBrush BackgroundLighterColorBrush = new SolidColorBrush(BackgroundLighterColor);
+        
+        public static Color BackgroundLightColor = Color.FromArgb(255, 56, 56, 56);
+        public static SolidColorBrush BackgroundLightColorBrush = new SolidColorBrush(BackgroundLightColor);
+    }
+}
