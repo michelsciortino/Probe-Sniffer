6e17023e0aa0f5ce3373a58204afe970b32e1c35
Michel Sciortino <michel.sciortino@outlook.com>
2018-05-26 13:32:53 +0200

    Addede the previously removed taskbar icon and toast window

--- server/ProbeSniffer/Program.cs
+++ server/ProbeSniffer/Program.cs
@@ -4,6 +4,11 @@ using Core.DBConnection;
 using Core.DeviceCommunication;
 using System.Windows;
 using Core.DataCollection;
+using System.IO;
+using ProbeSniffer.Windows;
+using System;
+using System.Windows.Input;
+using System.Reflection;
 
 namespace ProbeSniffer
 {
@@ -11,11 +16,13 @@ namespace ProbeSniffer
     {
         private Configuration configuration = null;
         private DataCollector dataCollector = null;
-        //private DeviceConnectionManager deviceCommunication = null;
+        private System.Windows.Forms.NotifyIcon notifyIcon = null;
+
 
         #region Windows
         private SplashScreen splash = null;
         private DataVisualizer visualizer = null;
+        private ToastMenu toast = null;
         #endregion
 
         /// <summary>
@@ -23,8 +30,6 @@ namespace ProbeSniffer
         /// </summary>
         public void Main()
         {
-            //bool result = false;
-
             //Showing Splash Screen
             splash = new SplashScreen();
             splash.Show();
@@ -66,8 +71,21 @@ namespace ProbeSniffer
 
             //Connecting to Devices
             splash.ShowDeviceAwaitingSplashScreen();
-            dataCollector.Initialize();
+            dataCollector = new DataCollector();
+            while(dataCollector.Initialize() is false);
             dataCollector.StartDataCollection();
+
+            //Setting up the notification icon
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            Stream iconStream = Assembly.GetAssembly(typeof(Core.Controls.MessageBox)).GetManifestResourceStream("Core.Resources.icon.ico");
+            notifyIcon.Icon = new System.Drawing.Icon(iconStream);
+            notifyIcon.Visible = true;
+            notifyIcon.Click += NotifyIcon_Click;
+            toast = new ToastMenu();
+            toast.MouseDoubleClick += Toast_MouseDoubleClick;
+            toast.Deactivated += MenuFlyout_Deactivated;
+            toast.ExitCLicked += Exit;
+            toast.ShowGraphClicked += Toast_ShowGraphClicked;
 
             //Opening visualizer
             splash.Close();
@@ -80,6 +98,30 @@ namespace ProbeSniffer
         {
             Core.Controls.MessageBox errorBox = new Core.Controls.MessageBox(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None);
             errorBox.Show();
+        }
+        private void Toast_ShowGraphClicked(object sender, RoutedEventArgs e) => visualizer.Show();
+
+        private void Exit(object sender, EventArgs e)
+        {
+            toast.Close();
+            visualizer.Close();
+            notifyIcon.Visible = false;
+            try { Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown; } catch { }
+            Application.Current.Shutdown();
+        }
+
+        private void MenuFlyout_Deactivated(object sender, EventArgs e) => toast.Hide();
+
+        private void Toast_MouseDoubleClick(object sender, MouseButtonEventArgs e) => toast.Hide();
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            if (toast is null) return;
+            toast.Left = SystemParameters.WorkArea.Width - toast.Width;
+            toast.Top = SystemParameters.WorkArea.Height - toast.Height;
+            toast.Opacity = 0;
+            toast.Show();
+            toast.Activate();
         }
 
     }
