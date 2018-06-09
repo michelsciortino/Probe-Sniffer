6653e71907406609803cf8cc4493c6dbdb580aec
Michel Sciortino <michel.sciortino@outlook.com>
2018-06-09 15:10:56 +0200

    Added log history and autoreset event

--- server/Core/Models/Logger.cs
+++ server/Core/Models/Logger.cs
@@ -8,13 +8,19 @@ namespace Core.Models
     {
         #region Private Members
         private static Queue<string> log = null;
+        private static List<string> historyLog=null;
         private static Mutex logMutex = null;
+
         #endregion
-        
+
+        public static AutoResetEvent NewLogMessage = null;
+
         static Logger()
         {
             log = new Queue<string>();
+            historyLog = new List<string>();
             logMutex = new Mutex();
+            NewLogMessage = new AutoResetEvent(false);
         }
 
         public static void Log(string message)
@@ -22,13 +28,13 @@ namespace Core.Models
             logMutex.WaitOne();
             try
             {
-                log.Enqueue("[" + DateTime.Now.ToShortTimeString() + "] " + message);
+                string entry = "[" + DateTime.Now.ToShortTimeString() + "] " + message;
+                log.Enqueue(entry);
+                historyLog.Add(entry);
             }
-            catch
-            {
-                log = new Queue<string>();
-            }
+            catch { log = new Queue<string>(); historyLog = new List<string>(); }
             logMutex.ReleaseMutex();
+            NewLogMessage.Set();
         }
 
         public static Queue<string> GetLogMessages()
@@ -38,11 +44,22 @@ namespace Core.Models
             try
             {
                 while (log.Count > 0) ret.Enqueue(log.Dequeue());
+
             }
             catch
             {
                 log = new Queue<string>();
             }
+            logMutex.ReleaseMutex();
+            return ret;
+        }
+
+        public static Queue<string> GetLogHistory()
+        {
+            Queue<string> ret = new Queue<string>();
+            logMutex.WaitOne();
+            foreach (var entry in historyLog) ret.Enqueue(entry);
+            log.Clear();
             logMutex.ReleaseMutex();
             return ret;
         }
