d4dfdad2b08ebb424a3f6afd70a5e621177c710e
Michel Sciortino <michel.sciortino@outlook.com>
2018-06-09 15:11:29 +0200

    Added MongoDB ObjectID property

--- server/Core/Models/Packet.cs
+++ server/Core/Models/Packet.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 
 namespace Core.Models
 {
     public class Packet
     {
+        public ObjectId _id { get; set; }
         public string MAC { get; set; }
         public string SSID { get; set; }
         public DateTime Timestamp { get; set; }
