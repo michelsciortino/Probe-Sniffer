b0a974e3706aa5bf4a4a00a959c951f39c881fc7
Michel Sciortino <michel.sciortino@outlook.com>
2018-10-02 12:25:30 +0200

    Old logger code deleted

--- server/Ui/ViewModels/DataVisualizer/DataVisualizationWindowViewModel.cs
+++ server/Ui/ViewModels/DataVisualizer/DataVisualizationWindowViewModel.cs
@@ -21,8 +21,6 @@ namespace Ui.ViewModels.DataVisualizer
         private readonly StatisticsViewModel _statisticsVM = null;
         private readonly HiddenDevicesViewModel _hiddenDevicesVM = null;
         private readonly SliderViewViewModel _sliderViewVM = null;
-        private Thread logger_updater;
-        private CancellationTokenSource loggerCancellation;
         #endregion
 
         #region Private Properties
@@ -40,24 +38,7 @@ namespace Ui.ViewModels.DataVisualizer
             _sliderViewVM = new SliderViewViewModel();
             _currentPage = new LiveViewView(_liveViewVM);
             _liveViewVM.RunUpdater();
-            Queue<string> history=Logger.GetLogHistory();
-            while(history.Count > 0) _loggerText += "\n" + history.Dequeue();
-            loggerCancellation = new CancellationTokenSource();
-            StartLogging();
         }
-        
-        public void StartLogging()
-        {
-            logger_updater = new Thread((x) => { UpdateLoggerText(loggerCancellation.Token); });
-            logger_updater.Start();
-        }
-
-        public void StopLogging()
-        {
-            loggerCancellation.Cancel();
-            logger_updater.Join();
-        }
-
         #endregion
 
         #region Public Properties
@@ -72,35 +53,11 @@ namespace Ui.ViewModels.DataVisualizer
             }
         }
 
-        public string LoggerText
-        {
-            get => _loggerText;
-        }
-
-        public string LogButtonText
-        {
-            get => (_loggerVisibility == Visibility.Visible) ? "Hide Logger" : "Show Logger";
-        }
-
-        public Visibility LoggerVisibility
-        {
-            get => _loggerVisibility;
-            set
-            {
-                if (_loggerVisibility == value)
-                    return;
-                _loggerVisibility = value;
-                OnPropertyChanged(nameof(LoggerVisibility));
-                OnPropertyChanged(nameof(LogButtonText));
-            }
-        }
-
         #endregion
 
         #region Private Commands
         private ICommand _selectionCommand = null;
         private ICommand _aboutCommand = null;
-        private ICommand _showHideLoggerCommand;
         #endregion
 
         #region Public Commands
@@ -108,8 +65,6 @@ namespace Ui.ViewModels.DataVisualizer
                                             (_selectionCommand = new RelayCommand<object>((x) => Navigate((SelectionChangedEventArgs)x)));
         public ICommand AboutCommand => _aboutCommand ??
                                         (_aboutCommand = new RelayCommand<object>((x) => ShowAbout()));
-        public ICommand ShowHideLoggerCommand => _showHideLoggerCommand ??
-                                                 (_showHideLoggerCommand = new RelayCommand<object>((x) => ShowHideLogger()));
         #endregion
 
         #region Private Methods
@@ -146,33 +101,6 @@ namespace Ui.ViewModels.DataVisualizer
         {
             Core.Controls.MessageBox message = new Core.Controls.MessageBox(AboutMessage, AboutCaption, MessageBoxButton.OK, MessageBoxImage.None, MessageBoxResult.None);
             message.Show();
-        }
-
-        private void ShowHideLogger()
-        {
-            if (_loggerVisibility == Visibility.Visible)
-                LoggerVisibility = Visibility.Collapsed;
-            else
-                LoggerVisibility = Visibility.Visible;
-        }
-
-        private void UpdateLoggerText(CancellationToken token)
-        {
-            while (token.IsCancellationRequested is false)
-            {
-                Logger.NewLogMessage.WaitOne(5000);
-                Queue<string> new_messages = Logger.GetLogMessages();
-                if (new_messages.Count == 0) continue;
-
-                Dispatcher.CurrentDispatcher.Invoke(() =>
-                {
-                    while (new_messages.Count > 0)
-                    {
-                        _loggerText += "\n" + new_messages.Dequeue();
-                    }
-                    OnPropertyChanged(nameof(LoggerText));
-                });
-            }
         }
         #endregion
     }
