8f3fa0233201ed6519224423371f56e0744544df
Michel Sciortino <michel.sciortino@outlook.com>
2018-10-02 12:09:23 +0200

    New Database data classes

--- server/Core/Models/Database/Probe.cs
+++ server/Core/Models/Database/Probe.cs
@@ -1,12 +1,9 @@
-﻿using MongoDB.Bson;
-using System;
+﻿using System;
 
 namespace Core.Models.Database
 {
-    public class IntervalDataEntry
+    public class Probe
     {
-        public ObjectId Id { get; set; }
-        public int IntervalId { get; set; }
         public Device Sender { get; set; }
         public string SSID { get; set; }
         public DateTime Timestamp { get; set; }
--- server/Core/Models/Database/ProbesInterval.cs
+++ server/Core/Models/Database/ProbesInterval.cs
@@ -1,15 +1,17 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 
 namespace Core.Models.Database
 {
-    public class IntervalDescriptionEntry
+    public class ProbesInterval
     {
+        [BsonId]
         public ObjectId Id { get; set; }
         public int IntervalId { get; set; }
-        public List<Device> ActiveEsps { get; set; }
-        public int NumberOfDetectedDevices { get; set; }
+        public List<ESP32_Device> ActiveEsps { get; set; }
+        public List<Probe> Probes { get; set; }
         public DateTime Timestamp { get; set; }
     }
 }
