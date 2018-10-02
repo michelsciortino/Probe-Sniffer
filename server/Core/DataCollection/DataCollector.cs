c3f31d00772788d98cf63b8a6a8582941306cfdc
Michel Sciortino <michel.sciortino@outlook.com>
2018-10-02 12:11:33 +0200

    Lots of here: datacollector and datastore threads joined + new interpolation thread

--- server/Core/DataCollection/DataCollector.cs
+++ server/Core/DataCollection/DataCollector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -7,6 +8,7 @@ using Core.DBConnection;
 using Core.DeviceCommunication;
 using Core.DeviceCommunication.Messages.ESP32_Messages;
 using Core.Models;
+using Core.Models.Database;
 
 namespace Core.DataCollection
 {
@@ -17,30 +19,26 @@ namespace Core.DataCollection
         private TcpServer server = null;
         private DatabaseConnection dbConnection = null;
         private Thread collectorThread = null;
-        private Thread dbStoreThread = null;
+        private Thread interplationThread = null;
         private CancellationTokenSource dataCollectionTokenSource = null;
-        private Queue<Packet> packets = null;
+        private Queue<Packet> toBeStored = null;
         private Queue<Packet> storeFails = null;
-        private Mutex packetsMutex = null;
         #endregion
-
-
+        
         #region Private Properties
         private bool _initialized = false;
         private bool _runnig = false;
         #endregion
 
         #region Constructor
-
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Await.Warning", "CS4014:Await.Warning")]
         public DataCollector()
         {
             dataCollectionTokenSource = new CancellationTokenSource();
             collectorThread = new Thread(() => DataCollectionLoop(dataCollectionTokenSource.Token));
-            dbStoreThread = new Thread(() => DataStoreLoopAsync(dataCollectionTokenSource.Token));
+            interplationThread = new Thread(() => InterpolationLoopAsync(dataCollectionTokenSource.Token));
             server = new TcpServer(ServerMode.DATACOLLECTION_MODE);
-            packets = new Queue<Packet>();
-            packetsMutex = new Mutex();
+            toBeStored = new Queue<Packet>();
             storeFails = new Queue<Packet>();
             dbConnection = new DatabaseConnection();
             dbConnection.Connect();
@@ -71,9 +69,21 @@ namespace Core.DataCollection
         public void StartDataCollection()
         {
             if (_initialized is false) return;
+            dbConnection.TestConnection();
+            while (true)
+            {
+                if (dbConnection.Connected is false)
+                { //Se la connessione è caduta, segnalo e riprovo dopo 10 secondi
+                    if (dbConnection.Connected is false)
+                        ShowErrorMessage("Database connection has gone down.\nPackets will be stored locally waiting for a new connection.");
+                    Thread.Sleep(10000);
+                    continue;
+                }
+                break;
+            }
             server.CanReceiveData = true;
             collectorThread.Start();
-            dbStoreThread.Start();
+            interplationThread.Start();
         }
 
         public void StopDataCollection()
@@ -83,84 +93,39 @@ namespace Core.DataCollection
             catch { }                
         }
 
-        private void DataCollectionLoop(CancellationToken token)
+        private async Task DataCollectionLoop(CancellationToken token)
         {
+            if (server.IsStarted is false) return;
             while (token.IsCancellationRequested is false)
             {
-                if (server.IsStarted)
-                {
-                    if (token.IsCancellationRequested) return;
-
-                    server.NewMessageEvent.WaitOne(2000);
-                    if (server.EnquedMessages == 0) continue;
-                    
-                    Data_Message message = server.GetNextMessage() as Data_Message;
-                    if (message is null) continue;
-
-                    DeviceData data = DeviceData.FromJson(message.Payload);
-                    if (data is null) continue;
-
-                    packetsMutex.WaitOne();
-                    foreach (Packet p in data.Packets)
-                    {
-                        p.ESP_MAC = data.Esp_Mac;
-                        packets.Enqueue(p);
-                    }
-                    packetsMutex.ReleaseMutex();
-                }
-            }
-        }
-
-        private async Task DataStoreLoopAsync(CancellationToken token)
-        {
-            Queue<Packet> toBeStored = new Queue<Packet>();
-            bool any;
-            while (true)
-            {
-                //Controllo la connessione con il database
-                int tries = 0;
-                while (tries < 3)
-                {
-                    if (dbConnection.Connected is true)
-                        break;
-                    else
-                        dbConnection.Connect();
-                    tries++;
-                }
-                if(dbConnection.Connected is false)
-                { //Se la connessione è caduta, segnalo e riprovo dopo secondi
+                dbConnection.TestConnection();
+                if (dbConnection.Connected is false)
+                { //Se la connessione è caduta, segnalo e riprovo dopo 10 secondi
                     if (dbConnection.Connected is false)
                         ShowErrorMessage("Database connection has gone down.\nPackets will be stored locally waiting for a new connection.");
-                    Thread.Sleep(5000);
+                    Thread.Sleep(10000);
                     continue;
                 }
 
-                //se ci sono dati non inviati al database, li reinserisco in coda
-                while (storeFails.Count > 0)
-                    toBeStored.Enqueue(storeFails.Dequeue());
-
-                packetsMutex.WaitOne();
-                any = packets.Count > 0;
-                packetsMutex.ReleaseMutex();
-
-                //Se non ci sono dati da salvare dormo 1 secondo
-                if(any is false && toBeStored.Count is 0)
-                {
-                    //Se la cancellazione è richiesta ritorno
-                    if (token.IsCancellationRequested is true)
-                        return;
-                    Thread.Sleep(1000);
-                    continue;
+                if (storeFails.Count == 0)
+                { // get new messages only if there are no pendents packets
+                    server.NewMessageEvent.WaitOne(2000);
+                    while (server.EnquedMessages > 0)
+                    { //ottengo tutti i nuovi messaggi dalla coda del server tcp
+                        Data_Message message = server.GetNextMessage() as Data_Message;
+                        if (message is null) continue;
+                        DeviceData data = DeviceData.FromJson(message.Payload);
+                        if (data is null) continue;
+                        foreach (Packet p in data.Packets)
+                        {
+                            p.ESP_MAC = data.Esp_Mac;
+                            toBeStored.Enqueue(p);
+                        }
+                    }
                 }
-
-                if (any)
-                {
-                    //Ottengo i nuovi dati
-                    packetsMutex.WaitOne();
-                    while (packets.Count > 0)
-                        toBeStored.Enqueue(packets.Dequeue());
-                    packetsMutex.ReleaseMutex();
-                }
+                else    //se ci sono dati non inviati al database, li reinserisco in coda			
+                    while (storeFails.Count > 0)
+                        toBeStored.Enqueue(storeFails.Dequeue());
 
                 //Invio i dati al database
                 while (toBeStored.Count > 0)
@@ -168,7 +133,7 @@ namespace Core.DataCollection
                     Packet p = toBeStored.Dequeue();
                     try
                     {
-                        bool result= await dbConnection.StorePacketAsync(p);
+                        bool result = await dbConnection.StorePacketAsync(p);
                         if (result is false)
                         {
                             storeFails.Enqueue(p);
@@ -181,9 +146,149 @@ namespace Core.DataCollection
                         break;
                     }
                 }
+
                 //Se ci sono dati non inviati al database, li inserisco nella lista di fail
                 while (toBeStored.Count > 0)
                     storeFails.Enqueue(toBeStored.Dequeue());
+            }
+        }
+
+        /// <summary>
+        /// This worker ask to the database for data to be processed
+        /// Only data created before the 5 minutes of each loop are taken into account, the newers can be calculated in the next loop.
+        /// If no data are avaible, the worker sleeps for one minute.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        private async Task InterpolationLoopAsync(CancellationToken token)
+        {
+            //Ottengo dal database l'ultimo intervallo salvato
+            ProbesInterval last_interval = null;
+            try
+            {
+                last_interval = await dbConnection.GetLastInterval();
+            }
+            catch
+            {
+                ShowErrorMessage("Database connection error.\nData Collection has been stopped");
+                return;
+            }
+            if(last_interval is null)
+            {
+                while (true)
+                {
+                    last_interval = new ProbesInterval
+                    {
+                        Timestamp = await dbConnection.GetFirstTimestamp(),
+                        IntervalId = -1
+                    };
+                    if(last_interval.Timestamp == default)
+                    {
+                        Thread.Sleep(30000);
+                        continue;
+                    }
+                    break;
+                }
+                last_interval.Timestamp = last_interval.Timestamp.AddSeconds(-last_interval.Timestamp.Second).AddMinutes(-1);
+            }
+
+            while (token.IsCancellationRequested is false)
+            {
+                if (DateTime.Compare(DateTime.UtcNow, last_interval.Timestamp.AddMinutes(5)) <0)
+                {
+                    Thread.Sleep(30000);
+                    continue;
+                }
+
+                ProbesInterval new_interval = new ProbesInterval
+                {
+                    IntervalId = last_interval.IntervalId + 1,
+                    Timestamp = last_interval.Timestamp.AddMinutes(1),
+                    Probes = new List<Probe>(),
+                    ActiveEsps = new List<ESP32_Device>()
+                };
+                
+
+                List<Packet> toBeProcessed = await dbConnection.GetRawData(new_interval.Timestamp,new_interval.Timestamp.AddMinutes(1));
+
+                //Per ogni pacchetto nell'intervallo ottengo un dizionario di <Hash pacchetto, Lista pacchetti con lo stesso hash> per avere tutte le rilevazioni
+                Dictionary<string, List<Packet>> DetectionForHash = new Dictionary<string, List<Packet>>();
+                foreach (Packet p in toBeProcessed)
+                {
+                    if (DetectionForHash.ContainsKey(p.Hash))
+                        DetectionForHash[p.Hash].Add(p);
+                    else
+                        DetectionForHash.Add(p.Hash, new List<Packet> { p });
+                }
+
+                //elimino dati che non servono più
+                toBeProcessed.Clear();
+                toBeProcessed = null;
+
+                //Elimino tutti i probe non detected da almeno 2 ESP
+                var toberemoved = DetectionForHash.Where(pair => pair.Value.Count < 2).Select(pair => pair.Key).ToList();
+                if (toberemoved != null)
+                {
+                    foreach (string hash in toberemoved)
+                        DetectionForHash.Remove(hash);
+                    toberemoved = null;
+                }
+                HashSet<string> esps = new HashSet<string>();
+                foreach (var detection in DetectionForHash)
+                {
+                    Point point = default;
+                    List<KeyValuePair<ESP32_Device, int>> detections = new List<KeyValuePair<ESP32_Device, int>>();
+                    foreach (Packet p in detection.Value)
+                        detections.Add(new KeyValuePair<ESP32_Device, int>(ESPManager.GetESPDevice(p.ESP_MAC), p.SignalStrength));
+
+                    try
+                    {
+                        point = Interpolator.Interpolate(detections);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    if (point != null)
+                    {
+                        if (point == default) continue;
+                        foreach (Packet p in detection.Value)
+                            esps.Add(p.ESP_MAC);
+                        new_interval.Probes.Add(new Probe()
+                        {
+                            SSID = detection.Value[0].SSID,
+                            Sender = new Device()
+                            {
+                                MAC = detection.Value[0].MAC,
+                                X_Position = point.X,
+                                Y_Position = point.Y
+                            },
+                            Timestamp = detection.Value[0].Timestamp,
+                            Hash = detection.Key
+                        });
+                    }
+                }
+                new_interval.ActiveEsps.AddRange(esps.Select((mac)=>ESPManager.GetESPDevice(mac)).ToList());
+
+                //Invio i dati al database
+                try
+                {
+                    bool result = await dbConnection.StoreProbesIntervalAsync(new_interval);
+                    if (result is false)
+                    {
+                        dataCollectionTokenSource.Cancel();
+                        ShowErrorMessage("Database connection error.\nData Collection has been stopped");
+                        return;
+                    }
+                }
+                catch
+                { //Se lo store del dato è fallito, segnalo e interrompo
+                    dataCollectionTokenSource.Cancel();
+                    ShowErrorMessage("Database connection error.\nData Collection has been stopped");
+                    return;
+                }
+                last_interval = new_interval;
             }
         }
         #endregion
