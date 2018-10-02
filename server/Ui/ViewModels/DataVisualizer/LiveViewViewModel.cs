dd52e9c8a7331b81952e7660568564e4d544145b
Michel Sciortino <michel.sciortino@outlook.com>
2018-10-02 12:28:16 +0200

    Updated for the new database structures + visual improvements

--- server/Ui/ViewModels/DataVisualizer/LiveViewViewModel.cs
+++ server/Ui/ViewModels/DataVisualizer/LiveViewViewModel.cs
@@ -11,20 +11,22 @@ using System.Linq;
 using Core.DataCollection;
 using System.Windows;
 using Core.DeviceCommunication;
+using Core.Models.Database;
 
 namespace Ui.ViewModels.DataVisualizer
 {
     public class LiveViewViewModel : BaseViewModel
     {
         #region Private Members
-        private const int UPDATING_RATE = 60000;
+        private const int UPDATING_RATE = 30000;
         private Timer timer = null;
         private DatabaseConnection dbConnection = null;
+        private DateTime last_interval_timestamp= default;
         #endregion
 
         #region Private Properties
-        private double _mapWidth=0;
-        private readonly double _mapHeight;
+        private double _mapWidth=20;
+        private double _mapHeight=20;
         #endregion
 
         #region Constructor
@@ -34,40 +36,71 @@ namespace Ui.ViewModels.DataVisualizer
             timer = new Timer((dispatcher) => UpdateAsync(dispatcher),Dispatcher.CurrentDispatcher,Timeout.Infinite,UPDATING_RATE);
             dbConnection = new DatabaseConnection();
             dbConnection.Connect();
-            #region TESTING
+            dbConnection.TestConnection();
             Devices = new ObservableCollection<Device>();
-            ESPDevices = new ObservableCollection<ESP32_Device>
-            {
-                new ESP32_Device() { X_Position = 0, Y_Position = 0, MAC = "00:00:00:00:00:00" },
-                new ESP32_Device() { X_Position = 400, Y_Position = 0, MAC = "00:00:00:00:00:00" },
-                new ESP32_Device() { X_Position = 0, Y_Position = 400, MAC = "00:00:00:00:00:00" },
-                new ESP32_Device() { X_Position = 400, Y_Position = 400, MAC = "00:00:00:00:00:00" }
-            };
-
-            foreach (Device d in Devices)
-            {
-                if (d.X_Position > MapWidth1) MapWidth1 = d.X_Position;
-                if (d.Y_Position > _mapHeight) _mapHeight = d.Y_Position;
-            }
-            foreach (Device d in ESPDevices)
-            {
-                if (d.X_Position > MapWidth1) MapWidth1 = d.X_Position;
-                if (d.Y_Position > _mapHeight) _mapHeight = d.Y_Position;
-            }
-
+            ESPDevices = new ObservableCollection<ESP32_Device>();
             Points = new ObservableCollection<KeyValuePair<int,string>>();
-            #endregion
         }
+
+        /*private async Task InitializeAsync()
+        {
+            *List<ProbesInterval> first_intervals = await dbConnection.GetLastNIntervals(20);
+            first_intervals.Sort((a, b) => a.Timestamp < b.Timestamp ? 1 : 0);
+
+            List<Probe> i_last = null;
+            foreach (var interval in first_intervals)
+            {
+                i_last = GetLastPositions(interval);
+                last_interval_timestamp = interval.Timestamp;
+                Points.Add(new KeyValuePair<int, string>(i_last.Count, interval.Timestamp.Hour.ToString() + ":" + interval.Timestamp.Minute.ToString()));
+            }
+            Devices.Clear();
+            foreach (Probe p in i_last)
+                Devices.Add(p.Sender);
+            ESPDevices.Clear();
+            foreach (ESP32_Device esp in first_intervals[first_intervals.Count-1].ActiveEsps)
+                ESPDevices.Add(esp);
+        }*/
         #endregion
 
         #region Public Properties
         public ObservableCollection<Device> Devices { get; set; }
         public ObservableCollection<ESP32_Device> ESPDevices { get; set; }
         public ObservableCollection<KeyValuePair<int, string>> Points { get; set; }
-        public Double MapWidth => MapWidth1 + 20; //added offset for device size
-        public Double MapHeight => _mapHeight + 20;
+        public Double MapHeight
+        {
+            get => _mapHeight;
+            set
+            {
+                _mapHeight = value + 20;
+                OnPropertyChanged(nameof(MapHeight));
+            }
+        }
+        public Double MapWidth
+        {
+            get => _mapWidth;
+            set
+            {
+                _mapWidth = value + 20;//offset
+                OnPropertyChanged(nameof(MapWidth));
+            }
+        }
+        #endregion
 
-        public double MapWidth1 { get => _mapWidth; set => _mapWidth = value; }
+        #region Private Methods
+        private List<Probe> GetLastPositions(ProbesInterval interval)
+        {
+            Dictionary<string, Probe> last = new Dictionary<string, Probe>();
+            foreach(var p in interval.Probes)
+            {
+                if (!last.ContainsKey(p.Sender.MAC)) last.Add(p.Sender.MAC, p);
+                else
+                {
+                    if (last[p.Sender.MAC].Timestamp < p.Timestamp) last[p.Sender.MAC] = p;
+                }
+            }
+            return last.Values.ToList();
+        }
         #endregion
 
         #region Public Methods
@@ -78,126 +111,50 @@ namespace Ui.ViewModels.DataVisualizer
             Dispatcher disp = dispatcher as Dispatcher;
             if(disp is null)
                 return;
-            DateTime old = DateTime.Now;
-            old=old.Subtract(new TimeSpan(0,2,0));
-            DateTime newd = DateTime.Now;
 
-            List<Packet> intervalPackets=await dbConnection.GetIntervalPacketAsync(old, newd);
-
-            //Per ogni pacchetto nell'intervallo ottengo un dizionario di <Hash pacchetto, Lista pacchetti con lo stesso hash>
-            Dictionary<string, List<Packet>> PacketForHash = new Dictionary<string, List<Packet>>();
-            foreach(Packet p in intervalPackets)
+            List<ProbesInterval> new_intervals = await dbConnection.GetLastIntervalsAfter(last_interval_timestamp);
+            if (new_intervals.Count == 0)
             {
-                if (PacketForHash.ContainsKey(p.Hash))
-                    PacketForHash[p.Hash].Add(p);
-                else
-                    PacketForHash.Add(p.Hash, new List<Packet> { p });
+                RunUpdater();
+                return;
             }
-
-            //elimino dati che non servono più
-            intervalPackets.Clear();
-
-            //Elimino tutti i probe non detected da almeno 2 ESP
-            Dictionary<string, List<Packet>> PurgedPacketForHash = new Dictionary<string, List<Packet>>();
-            foreach(var pair in PacketForHash)
-            {
-                if (pair.Value.Count >= 2)
-                    PurgedPacketForHash.Add(pair.Key,pair.Value);
-            }
-            PacketForHash = PurgedPacketForHash;
 
             //Per ogni mac di device trovato, prendo l'ultimo probe che questo ha generato nell'intervallo
-            Dictionary<string, string> latestHashForMac = new Dictionary<string, string>();
+            new_intervals.Sort((a, b) => DateTime.Compare(a.Timestamp,b.Timestamp));
 
-            foreach (string key in PacketForHash?.Keys)
+            List<Probe> i_last = null;
+            var new_points = new List<KeyValuePair<int, string>>();            
+            foreach (var interval in new_intervals)
             {
-                List<Packet> value = PacketForHash[key];
-                if (latestHashForMac.ContainsKey(value[0].MAC))
-                {
-                    if (PacketForHash[latestHashForMac[value[0].MAC]][0].Timestamp > value[0].Timestamp)
-                        latestHashForMac[value[0].MAC] = key;
-                }
-                else
-                    latestHashForMac.Add(value[0].MAC, key);
+                i_last = GetLastPositions(interval);
+                last_interval_timestamp = interval.Timestamp;
+                new_points.Add(new KeyValuePair<int, string>(i_last.Count, interval.Timestamp.Hour.ToString() + ":"+
+                                                            (interval.Timestamp.Minute<10?"0":"") + interval.Timestamp.Minute.ToString()+"\n"+
+                                                             interval.Timestamp.Date.ToShortDateString()));
             }
-
-            List<Device> newMapDevices = new List<Device>();
-
-            Dictionary<string,ESP32_Device> esps = ESPManager.ESPs.ToDictionary(e => e.MAC);
-            foreach (var pair in esps) pair.Value.Active = false;
-
-            foreach(KeyValuePair<string,string> pair in latestHashForMac)
-            {
-                Point point= default;
-                List<KeyValuePair<ESP32_Device, int>> detections = new List<KeyValuePair<ESP32_Device, int>>();
-                List<Packet> packets = PacketForHash[pair.Value];
-
-                foreach(Packet p in packets)
-                {
-                    detections.Add(new KeyValuePair<ESP32_Device, int>(esps[p.ESP_MAC], p.SignalStrength));
-                    if (esps[p.ESP_MAC].Active == false) esps[p.ESP_MAC].Active = true;
-                }
-
-
-                try
-                {
-                    point = Interpolator.Interpolate(detections);
-                }
-                catch
-                {
-                    continue;
-                }
-
-                newMapDevices.Add(new Device
-                {
-                    MAC = pair.Key,
-                    Timestamp = PacketForHash[pair.Value][0].Timestamp,
-                    X_Position = point.X,
-                    Y_Position = point.Y
-                });
-            }
-
-            latestHashForMac.Clear();
-            PacketForHash.Clear();
-
             disp.Invoke(() =>
             {
+                foreach (var p in new_points)
+                {
+                    if (Points.Count >= 20)
+                        Points.RemoveAt(0);
+                    Points.Add(p);
+                }
                 Devices.Clear();
-                foreach (Device d in newMapDevices)
-                    Devices.Add(d);
-                    ESPDevices.Clear();
-                foreach (ESP32_Device d in esps.Values)
-                    if(d.Active)
-                    ESPDevices.Add(d);
-                if (Points.Count >= 20)
-                    Points.RemoveAt(0);
-                Points.Add(new KeyValuePair<int, string>(Devices.Count, newd.Hour.ToString()+":"+newd.Minute.ToString()));
+                foreach (Probe p in i_last)
+                    Devices.Add(p.Sender);
+                ESPDevices.Clear();
+                foreach (ESP32_Device esp in new_intervals[new_intervals.Count - 1].ActiveEsps)
+                {
+                    if (MapWidth < esp.X_Position) MapWidth = esp.X_Position;
+                    if (MapHeight < esp.Y_Position) MapHeight = esp.Y_Position;
+                    ESPDevices.Add(esp);
+                }
             });
-
-            #region Test
-            /*
-            disp.Invoke(() => {
-                int Id;
-                if(Points.Count>=20)
-                    Points.RemoveAt(0);
-                if(Devices.Count > 50)
-                    Devices.RemoveAt(0);
-                Random r = new Random(DateTime.Now.Millisecond);
-                int newp = (int)(r.NextDouble() * 1000); if (newp < 0) newp = (-newp);
-                int newx = (int)(r.NextDouble() * 1000); if (newx < 0) newx = (-newx);
-                int newy = (int)(r.NextDouble() * 1000); if (newy < 0) newy = (-newy);
-                if (Points.Count == 0) Id = 0;
-                else Id = int.Parse(Points[Points.Count - 1].Value) + 1;
-                //Points.Add(new KeyValuePair<int, string>(0,(Id).ToString()));
-                Points.Add(new KeyValuePair<int, string>(newp % 250,(Id).ToString()));
-                Devices.Add(new Device { MAC = "AA:BB:CC:DD:EE:FF", X_Position= newx % 400 , Y_Position= newy%400  });
-            });
-            */
-            #endregion
             RunUpdater();
         }
 
-        public void RunUpdater() => timer.Change(UPDATING_RATE, UPDATING_RATE);
+        public void RunUpdater() => timer.Change(UPDATING_RATE/3, UPDATING_RATE);
         public void StopUpdater() => timer.Change(Timeout.Infinite, Timeout.Infinite);
         #endregion
     }
