808a3c907b41d0e989f32ecb0bd78f10810e78ff
Michel Sciortino <michel.sciortino@outlook.com>
2018-10-15 01:01:53 +0200

    New Statistics calcolator method

--- server/Ui/Models/DeviceStatistics.cs
+++ server/Ui/Models/DeviceStatistics.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Models.Database;
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,11 @@ using System.Windows.Media;
 
 namespace Ui.Models
 {
+    public enum Precision
+    {
+        HOUR,
+        DAY
+    }
     public class DeviceStatistics
     {
         public string MAC { get; set; }
@@ -14,5 +21,48 @@ namespace Ui.Models
         public bool Active { get; set; }
         public SolidColorBrush LineColor { get; set; }
         public int[] Probes { get; set; }
+
+        public static List<DeviceStatistics> DoStatistics(List<ProbesInterval> intervals, DateTime start, DateTime end, Precision precision)
+        {
+            int TotalDays = (int)(end - start).TotalDays;
+            int TotalHours = (int)(end - start).TotalHours;
+            Dictionary<string, DeviceStatistics> devicesStatistics = new Dictionary<string, DeviceStatistics>();
+            intervals = intervals.OrderBy(a => a.Timestamp).ToList();
+            foreach (var interval in intervals)
+            {
+                foreach (var p in interval.Probes)
+                {
+                    if (!devicesStatistics.ContainsKey(p.Sender.MAC))
+                    {
+                        DeviceStatistics d = new DeviceStatistics
+                        {
+                            MAC = p.Sender.MAC,
+                            LineColor = Styles.Colors.Next,
+                            Active = true,
+                            Tot_Probes = 0
+                        };
+                        if (precision == Precision.DAY)
+                        {
+                            d.Probes = new int[TotalDays];
+                            Array.Clear(d.Probes, 0, TotalDays);
+                        }
+                        else if (precision == Precision.HOUR)
+                        {
+                            d.Probes = new int[TotalHours];
+                            Array.Clear(d.Probes, 0, TotalHours);
+                        }
+                        devicesStatistics.Add(d.MAC, d);
+                    }
+
+                    if (precision == Precision.DAY)
+                        devicesStatistics[p.Sender.MAC].Probes[(int)(p.Timestamp - start).TotalDays] += 1;
+                    else if (precision == Precision.HOUR)
+                        devicesStatistics[p.Sender.MAC].Probes[(int)(p.Timestamp - start).TotalHours] += 1;
+
+                    devicesStatistics[p.Sender.MAC].Tot_Probes += 1;
+                }
+            }
+            return devicesStatistics.Values.ToList();
+        }
     }
 }
