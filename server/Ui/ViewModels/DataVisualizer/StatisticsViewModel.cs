0f168ad318951550d37439416aa235d85385a308
Michel Sciortino <michel.sciortino@outlook.com>
2018-10-15 23:08:19 +0200

    UncheckAll button added + Active/Deactive device statistics implemented

--- server/Ui/ViewModels/DataVisualizer/StatisticsViewModel.cs
+++ server/Ui/ViewModels/DataVisualizer/StatisticsViewModel.cs
@@ -23,7 +23,7 @@ namespace Ui.ViewModels.DataVisualizer
 
         #region Private Properties
         private ObservableCollection<DeviceStatistics> _devices = null;
-        private DateTime _startDate, _endDate;
+        private DateTime _startDate= DateTime.UtcNow, _endDate= DateTime.UtcNow.AddMinutes(1);
         private Precision _precision;
         private bool _isLoading = false, _hasData = false, _notFound = false;
         #endregion
@@ -37,6 +37,8 @@ namespace Ui.ViewModels.DataVisualizer
             Values = new ObservableRangeCollection<KeyValuePair<int[], SolidColorBrush>>();
             Labels = new ObservableRangeCollection<string>();
             _loadStatisticsCommand = new RelayCommand<object>((x) => LoadStatistics());
+            _uncheckAllCommand = new RelayCommand<object>((x) => UncheckAll());
+
         }
         #endregion
 
@@ -62,7 +64,10 @@ namespace Ui.ViewModels.DataVisualizer
             set
             {
                 if (_startDate == value) return;
-                _startDate = value;
+                if (DateTime.Compare(value, _endDate) >= 0)
+                    _startDate = _endDate.AddMinutes(-1);
+                else
+                    _startDate = value;
                 OnPropertyChanged(nameof(StartDate));
             }
         }
@@ -72,20 +77,19 @@ namespace Ui.ViewModels.DataVisualizer
             set
             {
                 if (_endDate == value) return;
-                _endDate = value;
+                if (DateTime.Compare(value, _startDate) <0)
+                    _endDate = _startDate.AddMinutes(1);
+                else
+                    _endDate = value;
                 OnPropertyChanged(nameof(EndDate));
             }
         }
-        public string Precision
+        public Precision Precision
         {
-            get => _precision.ToString();
+            get => _precision;
             set
             {
-                if (value == "Hour")
-                    _precision = Models.Precision.HOUR;
-
-                if (value == "Day")
-                    _precision = Models.Precision.DAY;
+                _precision = value;
                 OnPropertyChanged(nameof(Precision));
             }
         }
@@ -127,12 +131,29 @@ namespace Ui.ViewModels.DataVisualizer
 
         #region Private Commands
         private ICommand _loadStatisticsCommand = null;
+        private ICommand _uncheckAllCommand = null;
         #endregion
 
         #region Public Commands
         public ICommand LoadStatisticsCommand => _loadStatisticsCommand;
+        public ICommand UncheckAllCommand => _uncheckAllCommand;
         #endregion
         #region Private Methods
+
+        private void UncheckAll()
+        {
+            foreach (var d in Devices)
+            {
+                if (d.Active == false) continue;
+                d.PropertyChanged -= checkedChange;
+                d.Active = false;
+                d.PropertyChanged += checkedChange;
+            }
+            Values.Clear();
+        }
+        
+
+
         public async Task LoadStatistics()
         {
             var disp = Dispatcher.CurrentDispatcher;
@@ -167,13 +188,18 @@ namespace Ui.ViewModels.DataVisualizer
             }
             statistics.Sort((a, b) => a.Tot_Probes > b.Tot_Probes ? 1 : 0);
             statistics = statistics.OrderByDescending((a) => a.Tot_Probes).ToList();
+            foreach(var d in Devices) d.PropertyChanged -= checkedChange;
             Devices.Clear();
+
             foreach (var st in statistics)
+            {
+                st.PropertyChanged += checkedChange;
                 Devices.Add(st);
+            }
             Labels.Clear();
             Values.Clear();
             List<string> new_lables = new List<string>();
-            if (precision == Models.Precision.DAY)
+            if (precision == Models.Precision.Day)
             {
                 DateTime label = start.AddHours(-start.Hour).AddMinutes(-start.Minute).AddSeconds(-start.Second).AddMinutes(-start.Millisecond);
                 for (int i = (int)(end - start).TotalDays; i > 0; i--)
@@ -182,7 +208,7 @@ namespace Ui.ViewModels.DataVisualizer
                     label = label.AddDays(1);
                 }
             }
-            else if (precision == Models.Precision.HOUR)
+            else if (precision == Models.Precision.Hour)
             {
                 DateTime label = start.AddMinutes(-start.Minute).AddSeconds(-start.Second).AddMinutes(-start.Millisecond);
                 for (int i = (int)(end - start).TotalHours; i > 0; i--)
@@ -197,6 +223,12 @@ namespace Ui.ViewModels.DataVisualizer
             HasData = true;
             IsLoading = false;
             NotFound = false;
+        }
+
+        private void checkedChange(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Values.Clear();
+            Values.AddRange(_devices.Where((d) => d.Active is true).Select((d) => new KeyValuePair<int[], SolidColorBrush>(d.Probes, d.LineColor)));
         }
     }
 
