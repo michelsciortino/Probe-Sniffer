0ebcd1fae8c6be95b42663cc83a29f44b0df003c
Michel Sciortino <michel.sciortino@outlook.com>
2018-10-15 23:07:19 +0200

    Added PropertyChanged Event to track Active Property changes

--- server/Ui/Models/DeviceStatistics.cs
+++ server/Ui/Models/DeviceStatistics.cs
@@ -1,26 +1,41 @@
 ﻿using Core.Models.Database;
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media;
+using System.ComponentModel;
 
 namespace Ui.Models
 {
     public enum Precision
     {
-        HOUR,
-        DAY
+        Hour,
+        Day
     }
-    public class DeviceStatistics
+    public class DeviceStatistics:INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string MAC { get; set; }
         public int Tot_Probes { get; set; }
-        public bool Active { get; set; }
+        private bool _active=true;
         public SolidColorBrush LineColor { get; set; }
         public int[] Probes { get; set; }
+
+        public void SetPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool Active
+        {
+            get { return _active; }
+            set
+            {
+                _active = value;
+                SetPropertyChanged(nameof(Active));
+            }
+        }
 
         public static List<DeviceStatistics> DoStatistics(List<ProbesInterval> intervals, DateTime start, DateTime end, Precision precision)
         {
@@ -41,12 +56,12 @@ namespace Ui.Models
                             Active = true,
                             Tot_Probes = 0
                         };
-                        if (precision == Precision.DAY)
+                        if (precision == Precision.Day)
                         {
                             d.Probes = new int[TotalDays];
                             Array.Clear(d.Probes, 0, TotalDays);
                         }
-                        else if (precision == Precision.HOUR)
+                        else if (precision == Precision.Hour)
                         {
                             d.Probes = new int[TotalHours];
                             Array.Clear(d.Probes, 0, TotalHours);
@@ -54,9 +69,9 @@ namespace Ui.Models
                         devicesStatistics.Add(d.MAC, d);
                     }
 
-                    if (precision == Precision.DAY)
+                    if (precision == Precision.Day)
                         devicesStatistics[p.Sender.MAC].Probes[(int)(p.Timestamp - start).TotalDays] += 1;
-                    else if (precision == Precision.HOUR)
+                    else if (precision == Precision.Hour)
                         devicesStatistics[p.Sender.MAC].Probes[(int)(p.Timestamp - start).TotalHours] += 1;
 
                     devicesStatistics[p.Sender.MAC].Tot_Probes += 1;
