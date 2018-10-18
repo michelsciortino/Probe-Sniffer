9ba56f129a04e9d1b0d62846f294ce09c790a8dd
Michel Sciortino <michel.sciortino@outlook.com>
2018-10-19 00:00:46 +0200

    Some properties changes

--- server/Ui/Controls/DevicesMap/DevicesMap.xaml.cs
+++ server/Ui/Controls/DevicesMap/DevicesMap.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using Core.Models;
 using Core.Models.Database;
+using Ui.Models;
 
 namespace Ui.Controls.DevicesMap
 {
@@ -30,29 +31,29 @@ namespace Ui.Controls.DevicesMap
                 Margin = new Thickness(0),
                 Padding = new Thickness(0),
                 Text = "\xE704",
-                FontSize = 12,
+                FontSize = 20,
                 FontFamily = new FontFamily("Segoe MDL2 Assets"),
                 Foreground = ESPsColor,
                 ToolTip = New_Device_ToolTip(esp.MAC, esp.X_Position.ToString(), esp.Y_Position.ToString())
             };
         }
 
-        private UIElement New_Device(Probe probe)
+        private UIElement New_Device(DeviceStatistic deviceStatistic)
         {
             Grid container = new Grid();
             Border background = new Border();
             background.Background = new SolidColorBrush(Color.FromRgb(32,32,32));
             background.BorderThickness = new Thickness(0);
             background.CornerRadius = new CornerRadius(2);
-            background.Margin = new Thickness(2.5,.75,2,.75);
+            background.Margin = new Thickness(3.5,.75,3.5,.75);
             TextBlock t = new TextBlock();
             t.Margin = new Thickness(0);
             t.Padding = new Thickness(0);
             t.Text = "\xE8EA";
-            t.FontSize = 12;
+            t.FontSize = 20;
             t.FontFamily = new FontFamily("Segoe MDL2 Assets");
-            t.Foreground = DevicesColor;
-            t.ToolTip = New_Device_ToolTip(probe.Sender.MAC, probe.Sender.X_Position.ToString(), probe.Sender.Y_Position.ToString(), probe.SSID);
+            t.Foreground = deviceStatistic.Color is null?DevicesColor:deviceStatistic.Color;
+            t.ToolTip = New_Device_ToolTip(deviceStatistic.MAC, deviceStatistic.X_Position.ToString(), deviceStatistic.Y_Position.ToString(), deviceStatistic.SSID);
             container.Children.Add(background);
             container.Children.Add(t);
             return container;
@@ -154,12 +155,11 @@ namespace Ui.Controls.DevicesMap
             set { SetValue(ESPsProperty, value); }
         }
 
-        public ObservableCollection<Probe> Probes
+        public ObservableCollection<DeviceStatistic> Devices
         {
-            get { return (ObservableCollection<Probe>)GetValue(ProbesProperty); }
-            set { SetValue(ProbesProperty, value); }
+            get { return (ObservableCollection<DeviceStatistic>)GetValue(DevicesProperty); }
+            set { SetValue(DevicesProperty, value); }
         }
-
         public double MapHeight
         {
             get { return (double)GetValue(MapHeightProperty); }
@@ -188,8 +188,8 @@ namespace Ui.Controls.DevicesMap
         #region Dependency Properties
         public static readonly DependencyProperty ESPsProperty =
             DependencyProperty.Register("ESPs", typeof(ObservableCollection<ESP32_Device>), typeof(DevicesMap), new FrameworkPropertyMetadata(null, OnMapChanged));
-        public static readonly DependencyProperty ProbesProperty =
-            DependencyProperty.Register("Probes", typeof(ObservableCollection<Probe>), typeof(DevicesMap), new FrameworkPropertyMetadata(null, OnMapChanged));
+        public static readonly DependencyProperty DevicesProperty =
+            DependencyProperty.Register("Devices", typeof(ObservableCollection<DeviceStatistic>), typeof(DevicesMap), new FrameworkPropertyMetadata(null, OnMapChanged));
         public static readonly DependencyProperty MapHeightProperty =
             DependencyProperty.Register("MapHeight", typeof(double), typeof(DevicesMap), new PropertyMetadata((double)200));
         public static readonly DependencyProperty MapWidthProperty =
@@ -210,9 +210,9 @@ namespace Ui.Controls.DevicesMap
             if (e.NewValue is ObservableCollection<ESP32_Device> new_ESPs)
                 new_ESPs.CollectionChanged += map.OnESPsCollectionChanged;
 
-            if (e.OldValue is ObservableCollection<Probe> old_)
+            if (e.OldValue is ObservableCollection<DeviceStatistic> old_)
                 old_.CollectionChanged -= map.OnDevicesCollectionChanged;
-            if (e.NewValue is ObservableCollection<Probe> new_)
+            if (e.NewValue is ObservableCollection<DeviceStatistic> new_)
                 new_.CollectionChanged += map.OnDevicesCollectionChanged;
 
             map.ESPCollection.Clear();
@@ -229,18 +229,19 @@ namespace Ui.Controls.DevicesMap
                 }
             }
             map.DevicesCollection.Clear();
-            if (map.Probes != null)
+            if (map.Devices != null)
             {
-                foreach (Probe p in map.Probes)
+                foreach (var d in map.Devices)
                 {
-                    UIElement ESP = map.New_Device(p);
-                    Canvas.SetLeft(ESP, p.Sender.X_Position);
-                    Canvas.SetBottom(ESP, p.Sender.Y_Position);
-                    map.DevicesCollection.Add(ESP);
+                    UIElement device = map.New_Device(d);
+                    Canvas.SetLeft(device, d.X_Position);
+                    Canvas.SetBottom(device, d.Y_Position);
+                    map.DevicesCollection.Add(device);
+                    if (map.MapHeight < d.Y_Position + 20) map.MapHeight = d.Y_Position + 20;
+                    if (map.MapWidth < d.X_Position + 20) map.MapWidth = d.X_Position + 20;
                 }
             }
-            map.UpdateMap();
-
+            map.UpdateMap();            
         }
 
         private void OnESPsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
@@ -266,12 +267,14 @@ namespace Ui.Controls.DevicesMap
             DevicesCollection.Clear();
             if (e.NewItems != null)
             {
-                foreach (Probe p in Probes)
+                foreach (var d in Devices)
                 {
-                    UIElement ESP = New_Device(p);
-                    Canvas.SetLeft(ESP, p.Sender.X_Position);
-                    Canvas.SetBottom(ESP, p.Sender.Y_Position);
-                    DevicesCollection.Add(ESP);
+                    UIElement device = New_Device(d);
+                    Canvas.SetLeft(device, d.X_Position);
+                    Canvas.SetBottom(device, d.Y_Position);
+                    DevicesCollection.Add(device);
+                    if (MapHeight < d.Y_Position + 20) MapHeight = d.Y_Position + 20;
+                    if (MapWidth < d.X_Position + 20) MapWidth = d.X_Position + 20;
                 }
             }
             UpdateMap();
@@ -284,6 +287,8 @@ namespace Ui.Controls.DevicesMap
                 DevicesContainer.Children.Add(el);
             foreach (UIElement el in ESPCollection)
                 DevicesContainer.Children.Add(el);
+            Width = MapWidth;
+            Height = MapHeight;
         }
 
         #endregion
